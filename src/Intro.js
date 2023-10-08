cad448d2a19ec4db0284f3ac197c8ab0b96750a3
Pedro Arthur Alves de oliveira <pedro.sistemas@intranet.lar.ind.br>
2023-10-08 16:36:01 -0300

    Initial commit

---
+++ src/Intro.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Intro.css";
+
+const Intro = () => {
+    return (
+        <div class="hero">
+            <h1>
+                Olá, bem-vindos ao site de captação de fundos para o projeto da Kombi missionária!
+            </h1>
+        </div>
+    );
+};
+
+export default Intro;
